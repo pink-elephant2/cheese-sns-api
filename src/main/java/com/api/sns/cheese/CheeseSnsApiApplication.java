dd44b0679301d25d32b699dcae4b4653012a1b96
matyahiko2831 <matyahiko2831@gmail.com>
2019-03-30 23:47:27 +0900

    DB疎通

--- src/main/java/com/api/sns/cheese/CheeseSnsApiApplication.java
+++ src/main/java/com/api/sns/cheese/CheeseSnsApiApplication.java
@@ -1,9 +1,11 @@
 package com.api.sns.cheese;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.api.sns.cheese.repository")
 public class CheeseSnsApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/api/sns/cheese/api/v1/HelloController.java
+++ src/main/java/com/api/sns/cheese/api/v1/HelloController.java
@@ -1,15 +1,35 @@
 package com.api.sns.cheese.api.v1;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.sns.cheese.domain.Test;
+import com.api.sns.cheese.domain.TestExample;
+import com.api.sns.cheese.repository.TestMapper;
+
 @RestController
 public class HelloController {
+
+	@Autowired
+	private TestMapper testMapper;
 
 	@RequestMapping("/")
     @ResponseBody
     public String home() {
         return "Hello, World!";
     }
+
+	@RequestMapping("/sqlsample")
+    @ResponseBody
+	public List<Test> sqlSample(){
+
+		TestExample example = new TestExample();
+		example.createCriteria().andIdIsNotNull();
+
+		return testMapper.selectByExample(example);
+	}
 }
