683f232b3016be2b4158a72c095385bd45a85038
c5apple <c5apple_party@yahoo.co.jp>
2019-04-21 14:59:33 +0900

    お問合せ DB登録

--- src/main/java/com/api/sns/cheese/service/ContactServiceImpl.java
+++ src/main/java/com/api/sns/cheese/service/ContactServiceImpl.java
@@ -1,9 +1,15 @@
 package com.api.sns.cheese.service;
 
+import java.util.Date;
+
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.api.sns.cheese.domain.TContact;
 import com.api.sns.cheese.form.ContactForm;
+import com.api.sns.cheese.repository.TContactMapper;
 
 /**
  * お問合せサービス
@@ -11,6 +17,12 @@ import com.api.sns.cheese.form.ContactForm;
 @Service
 @Transactional
 public class ContactServiceImpl implements ContactService {
+
+	@Autowired
+	private TContactMapper tContactMapper;
+
+	@Autowired
+	private Mapper mapper;
 
 	/**
 	 * お問合せ登録する
@@ -21,7 +33,17 @@ public class ContactServiceImpl implements ContactService {
 	@Override
 	public boolean save(ContactForm form) {
 
-		// TODO DB登録
+		// DB登録
+		TContact contact = mapper.map(form, TContact.class);
+		contact.setReadFlag(false);
+
+		// TODO 共通項目は親クラスで設定する
+		contact.setDeleted("0");
+		contact.setCreatedAt(new Date());
+		contact.setCreatedBy(1); // TODO システムユーザ(1) TODO ログイン済みならそのユーザ
+		contact.setUpdatedAt(new Date());
+		contact.setUpdatedBy(1); // TODO システムユーザ(1) TODO ログイン済みならそのユーザ
+		tContactMapper.insert(contact);
 
 		// TODO 運営にメール送信
 
