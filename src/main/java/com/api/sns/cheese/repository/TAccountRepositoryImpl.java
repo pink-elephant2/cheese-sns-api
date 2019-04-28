36164ce00267d1c168e70a96e1c6d616ba1f7dc1
c5apple <c5apple_party@yahoo.co.jp>
2019-04-28 10:39:28 +0900

    #31 リポジトリ適用

--- src/main/java/com/api/sns/cheese/repository/TAccountRepositoryImpl.java
+++ src/main/java/com/api/sns/cheese/repository/TAccountRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.api.sns.cheese.repository;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
+@Primary
 @Repository
 public class TAccountRepositoryImpl implements TAccountRepository {
     private TAccountMapper tAccountMapper;
