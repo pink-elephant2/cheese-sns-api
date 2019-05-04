e8dc03c5141060f98a8c92afb5193cbf44aaf3a7
c5apple <c5apple_party@yahoo.co.jp>
2019-05-05 05:43:34 +0900

    タイムゾーン修正

--- src/main/java/com/api/sns/cheese/resources/PhotoResource.java
+++ src/main/java/com/api/sns/cheese/resources/PhotoResource.java
@@ -3,7 +3,6 @@ package com.api.sns.cheese.resources;
 import java.util.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
@@ -32,7 +31,6 @@ public class PhotoResource {
 	private String imgUrl;
 
 	/** 投稿日時 */
-	@JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
 	private Date createdAt;
 
 	/** 投稿ユーザー */
